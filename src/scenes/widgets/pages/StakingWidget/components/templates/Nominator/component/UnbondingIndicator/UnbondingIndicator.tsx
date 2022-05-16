e9e05702b2a4cfe1ba2e623046c4943e75e211ca
yordan_Ip <yordangdanchev@gmail.com>
2022-05-16 14:50:10 +0300

    - derive unlocking progress

--- src/scenes/widgets/pages/StakingWidget/components/templates/Nominator/component/UnbondingIndicator/UnbondingIndicator.tsx
+++ src/scenes/widgets/pages/StakingWidget/components/templates/Nominator/component/UnbondingIndicator/UnbondingIndicator.tsx
@@ -2,7 +2,7 @@ import React, {FC} from 'react';
 
 import {IconSvg, PropsWithThemeInterface, Tooltip} from 'ui-kit';
 import {useStore} from 'shared/hooks';
-import SubstrateProvider from "shared/services/web3/SubstrateProvider";
+import SubstrateProvider from 'shared/services/web3/SubstrateProvider';
 
 import * as styled from './UnbondingIndicator.styled';
 
@@ -12,7 +12,7 @@ export const UnbondingIndicator: FC<PropsInterface> = ({theme}) => {
   const {widgetStore} = useStore();
   const {stakingInfo, sessionProgress} = widgetStore.stakingStore.polkadotProviderStore;
   const progress = SubstrateProvider.deriveUnbondingProgress(stakingInfo, sessionProgress);
-  console.log(progress)
+  console.log(progress);
   return (
     <Tooltip label={<></>} placement="left" darkBackground size={{width: '200px'}}>
       <styled.UnbondingIndicator>
