0be5cc54bbb46910bb0430d62f4b50e81e7b50be
Daniel Aboo <daboo1998@gmail.com>
2022-05-18 10:40:03 +0200

    Linked help section to onboarding, removed legacy tutorial, and reordered help sections

--- src/scenes/AppLayers.tsx
+++ src/scenes/AppLayers.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useContext, useRef, useState} from 'react';
+import React, {FC, useState} from 'react';
 import {observer} from 'mobx-react-lite';
 import {toast} from 'react-toastify';
 import {useAuth} from 'react-oidc-context';
@@ -12,14 +12,8 @@ import UnityLoading from '../component/atoms/UnityLoading';
 import CommunicationLayer from '../component/overlays/CommunicationLayer';
 import InFlightControlLayer from '../component/overlays/InFlightControlLayer';
 import LiveStreamLayer from '../component/overlays/LiveStreamLayer';
-import WelcomePopup from '../component/popup/welcome/WelcomePopup';
-import Modal, {ModalRef} from '../component/util/Modal';
 import useUnityEvent from '../context/Unity/hooks/useUnityEvent';
 import UnityService from '../context/Unity/UnityService';
-import {AgoraContext} from '../context/AgoraContext';
-import TutorialPopup from '../component/popup/welcome/TutorialPopup';
-import VideoAndAudioSettingsPopup from '../component/popup/welcome/VideoAndAudioSettingsPopup';
-import ReadyToLiftOffPopup from '../component/popup/welcome/ReadyToLiftOffPopup';
 import VideoLayer from '../component/overlays/VideoLayer';
 import SocialLayout from '../component/layout/SocialUI/SocialLayout';
 import MusicPlayerLayout from '../component/layout/MusicPlayer/MusicPlayerLayout';
@@ -28,27 +22,16 @@ const AppLayers: FC = ({children}) => {
   const theme = useTheme();
   const {
     favoriteStore,
-    mainStore: {worldStore, unityStore},
-    widgetStore: {helpStore}
+    mainStore: {worldStore, unityStore}
   } = useStore();
 
   const [loading, setLoading] = useState(true);
   const auth = useAuth();
-  const {getMicrophoneConsent, getCameraConsent} = useContext(AgoraContext);
-
-  const welcomeModal = useRef<ModalRef>(null);
-  const tutorialModal = useRef<ModalRef>(null);
-  const settingsModal = useRef<ModalRef>(null);
-  const liftOffModal = useRef<ModalRef>(null);
 
   useUnityEvent('MomentumLoaded', () => {
     console.info('MomentumLoaded');
     favoriteStore.fetchFavorites();
     UnityService.setAuthToken(auth.user?.access_token);
-    if (!localStorage.getItem('no-welcome')) {
-      welcomeModal.current?.open();
-      helpStore.helpDialog.open();
-    }
   });
 
   useUnityEvent('TeleportReady', () => {
@@ -106,79 +89,6 @@ const AppLayers: FC = ({children}) => {
 
       <VideoLayer />
       <LiveStreamLayer />
-
-      {/* TODO: Move modals to AppModals.tsx */}
-      <Modal ref={welcomeModal}>
-        <WelcomePopup
-          onReadyToFly={() => {
-            welcomeModal.current?.close();
-            localStorage.setItem('no-welcome', '1');
-          }}
-          onQuickIntro={() => {
-            welcomeModal.current?.close();
-            tutorialModal.current?.open();
-          }}
-          onClose={() => {
-            welcomeModal.current?.close();
-            getMicrophoneConsent().then((consent) => {
-              if (consent) {
-                settingsModal.current?.open();
-              }
-
-              return getCameraConsent();
-            });
-          }}
-        />
-      </Modal>
-      <Modal ref={tutorialModal}>
-        <TutorialPopup
-          onClose={() => {
-            tutorialModal.current?.close();
-          }}
-          onPrevious={() => {
-            tutorialModal.current?.close();
-            welcomeModal.current?.open();
-          }}
-          onNext={() => {
-            tutorialModal.current?.close();
-            getMicrophoneConsent().then((consent) => {
-              if (consent) {
-                settingsModal.current?.open();
-              }
-            });
-          }}
-        />
-      </Modal>
-      <Modal ref={settingsModal}>
-        <VideoAndAudioSettingsPopup
-          onClose={() => {
-            settingsModal.current?.close();
-          }}
-          onPrevious={() => {
-            settingsModal.current?.close();
-            tutorialModal.current?.open();
-          }}
-          onNext={() => {
-            settingsModal.current?.close();
-            liftOffModal.current?.open();
-          }}
-        />
-      </Modal>
-      <Modal ref={liftOffModal}>
-        <ReadyToLiftOffPopup
-          onClose={() => {
-            liftOffModal.current?.close();
-          }}
-          onPrevious={() => {
-            liftOffModal.current?.close();
-            settingsModal.current?.open();
-          }}
-          onLiftOff={() => {
-            liftOffModal.current?.close();
-            localStorage.setItem('no-welcome', '1');
-          }}
-        />
-      </Modal>
     </>
   );
 };
