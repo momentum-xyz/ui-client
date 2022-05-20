0dfa6ae1148b23ea782bb218f9115d3af464647d
andrey_kovalev_s <andrey.kovalev@perfectart.com>
2022-05-20 11:19:02 +0300

    auth: 720p

--- src/scenes/auth/pages/Web3ChoicePage/Web3ChoicePage.styled.ts
+++ src/scenes/auth/pages/Web3ChoicePage/Web3ChoicePage.styled.ts
@@ -10,12 +10,3 @@ export const Background = styled.div`
   justify-content: center;
   z-index: 1;
 `;
-
-export const Networks = styled.div`
-  height: 100%;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 40px;
-  align-items: center;
-  justify-content: center;
-`;
