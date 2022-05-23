24fe3a7266b69cf53b58a455f0cae53f94bfc3f3
andrey_kovalev_s <andrey.kovalev@perfectart.com>
2022-05-23 22:23:04 +0300

    intro: page

--- src/scenes/default/pages/IntroPage/IntoPage.tsx
+++ src/scenes/default/pages/IntroPage/IntoPage.tsx
@@ -1,10 +1,53 @@
-import React, {FC} from 'react';
-import {observer} from 'mobx-react-lite';
+import React, {FC, useCallback, useEffect} from 'react';
+import {useHistory} from 'react-router';
+import YouTube from 'react-youtube';
+
+import {cookie} from 'core/services';
+import {CookieKeyEnum} from 'core/enums';
+import {SvgButton} from 'ui-kit';
+import {ROUTES} from 'core/constants';
+import background from 'static/images/bg.png';
 
 import * as styled from './IntoPage.styled';
 
+// FIXME: Move to constants
+const YOUTUBE_INTRO_VIDEO_ID = '0ZctdA2SxKk';
+
 const IntoPage: FC = () => {
-  return <styled.Div>Video.</styled.Div>;
+  const history = useHistory();
+
+  useEffect(() => {
+    if (!cookie.has(CookieKeyEnum.INTRO)) {
+      cookie.create(CookieKeyEnum.INTRO, '1');
+    }
+  }, []);
+
+  const closeHandler = useCallback(() => {
+    history.push(ROUTES.base);
+  }, [history]);
+
+  const opts = {
+    playerVars: {
+      autoplay: 1,
+      mute: 1,
+      controls: 1
+    }
+  };
+
+  return (
+    <styled.Background background={background}>
+      <styled.CloseIcon>
+        <SvgButton iconName="close" size="medium-large" isWhite onClick={closeHandler} />
+      </styled.CloseIcon>
+      <YouTube
+        videoId={YOUTUBE_INTRO_VIDEO_ID}
+        onEnd={closeHandler}
+        className="youtube"
+        iframeClassName="youtubeIframe"
+        opts={opts}
+      />
+    </styled.Background>
+  );
 };
 
-export default observer(IntoPage);
+export default IntoPage;
