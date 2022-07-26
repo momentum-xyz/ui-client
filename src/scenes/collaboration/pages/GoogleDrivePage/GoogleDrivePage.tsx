1411896c30f66b52e6b232cf6e6d1fa5e9964bd0
andrey_kovalev_s <andrey.kovalev@perfectart.com>
2022-07-26 15:21:19 +0300

    feature(space): miro integration

--- src/scenes/collaboration/pages/GoogleDrivePage/GoogleDrivePage.tsx
+++ src/scenes/collaboration/pages/GoogleDrivePage/GoogleDrivePage.tsx
@@ -1,11 +1,11 @@
 import React, {FC} from 'react';
 import {observer} from 'mobx-react-lite';
 
-import GoogleDriveLayout from "component/layout/Collaboration/GoogleDriveLayout";
+import GoogleDriveLayout from 'component/layout/Collaboration/GoogleDriveLayout';
 
 // TODO: Refactor like "MiroBoardPage"
 const GoogleDrivePage: FC = () => {
-  return <GoogleDriveLayout />
+  return <GoogleDriveLayout />;
 };
 
 export default observer(GoogleDrivePage);
