d240fc0ba2f0186dba58aa82b6630a4c651c1064
andrey_kovalev_s <andrey.kovalev@perfectart.com>
2022-08-12 13:39:29 +0300

    feature(meeting): small changes

--- src/scenes/meeting/pages/MeetingRoomPage/MeetingRoomPage.tsx
+++ src/scenes/meeting/pages/MeetingRoomPage/MeetingRoomPage.tsx
@@ -1,6 +1,5 @@
 import React, {FC, useEffect} from 'react';
 import {observer} from 'mobx-react-lite';
-import {Transition} from '@headlessui/react';
 
 import {useStore} from 'shared/hooks';
 
@@ -27,76 +26,68 @@ const MeetingRoomPage: FC = () => {
     meetingStore.setKicked(false);
   }, [stageModeStore, meetingStore]);
 
+  if (!agoraStore.hasJoined) {
+    return <></>;
+  }
+
   return (
-    <Transition
-      show={agoraStore.hasJoined}
-      unmount={false}
-      className="z-main-u ml-auto mr-1 "
-      enter="transform transition-transform ease-out duration-300"
-      enterFrom="translate-x-5 "
-      enterTo="translate-x-0 "
-      leave="transform transition-transform ease-in duration-300"
-      leaveFrom="translate-x-0 "
-      leaveTo="translate-x-5 "
-    >
-      <styled.Container data-testid="MeetingRoomPage-test">
-        <styled.ListItem>
-          <JoinLeaveButtons isShown={!unityStore.isPaused} />
+    <styled.Container data-testid="MeetingRoomPage-test">
+      <styled.ListItem>
+        <JoinLeaveButtons isShown={!unityStore.isPaused} />
 
-          <styled.ListItemContent className="noScrollIndicator">
-            <PeopleCount count={agoraStore.meetingPeopleCount} />
+        <styled.ListItemContent className="noScrollIndicator">
+          <PeopleCount count={agoraStore.meetingPeopleCount} />
 
-            <ul>
-              {/* MUTE ALL */}
-              <MuteAllButton
-                isShown={!agoraStore.isStageMode && collaborationStore.isModerator}
-                peopleCount={agoraStore.meetingPeopleCount}
-                onMuteAll={() => meetingRoomStore.muteAllUsers(space?.id)}
-              />
+          <ul>
+            {/* MUTE ALL */}
+            <MuteAllButton
+              isShown={!agoraStore.isStageMode && collaborationStore.isModerator}
+              peopleCount={agoraStore.meetingPeopleCount}
+              onMuteAll={() => meetingRoomStore.muteAllUsers(space?.id)}
+            />
 
-              {/* CURRENT USER */}
-              <LocalUser
-                isShown={!agoraStore.isStageMode || !agoraStageModeStore.isOnStage}
-                isStageMode={agoraStore.isStageMode}
-                avatarSrc={sessionStore.profile?.avatarSrc}
-                videoTrack={userDevicesStore.localVideoTrack}
-                microphoneOff={userDevicesStore.muted}
-                cameraOff={userDevicesStore.cameraOff}
-                soundLevel={agoraStore.localSoundLevel}
-              />
+            {/* CURRENT USER */}
+            <LocalUser
+              isShown={!agoraStore.isStageMode || !agoraStageModeStore.isOnStage}
+              isStageMode={agoraStore.isStageMode}
+              avatarSrc={sessionStore.profile?.avatarSrc}
+              videoTrack={userDevicesStore.localVideoTrack}
+              microphoneOff={userDevicesStore.muted}
+              cameraOff={userDevicesStore.cameraOff}
+              soundLevel={agoraStore.localSoundLevel}
+            />
 
-              <MaxVideoStreams isShown={agoraMeetingStore.maxVideoStreamsReached} />
+            <MaxVideoStreams isShown={agoraMeetingStore.maxVideoStreamsReached} />
 
-              {/* STAGE MODE USERS OR MEETING USERS */}
-              {agoraStore.isStageMode
-                ? agoraStageModeStore.audienceMembers.map((user) => (
-                    <StageModeUser
-                      key={user.uid}
-                      user={user}
-                      isModerator={collaborationStore.isModerator}
-                      canEnterStage={agoraStageModeStore.canEnterStage}
-                      inviteToStage={agoraStageModeStore.inviteToStage}
-                      isInviteDialogShown={collaborationStore.inviteOnStageDialog.isOpen}
-                      openInviteDialog={collaborationStore.inviteOnStageDialog.open}
-                      closeInviteDialog={collaborationStore.inviteOnStageDialog.close}
-                    />
-                  ))
-                : agoraMeetingStore.users.map((user) => (
-                    <MeetingUser
-                      key={user.uid}
-                      spaceId={space?.id || ''}
-                      user={user}
-                      isModerator={collaborationStore.isModerator}
-                      maxVideoStreams={agoraMeetingStore.maxVideoStreamsReached}
-                      onMuteUser={meetingRoomStore.muteUser}
-                      onKickUser={meetingRoomStore.kickUser}
-                    />
-                  ))}
-            </ul>
-          </styled.ListItemContent>
-        </styled.ListItem>
-      </styled.Container>
-    </Transition>
+            {/* STAGE MODE USERS OR MEETING USERS */}
+            {agoraStore.isStageMode
+              ? agoraStageModeStore.audienceMembers.map((user) => (
+                  <StageModeUser
+                    key={user.uid}
+                    user={user}
+                    isModerator={collaborationStore.isModerator}
+                    canEnterStage={agoraStageModeStore.canEnterStage}
+                    inviteToStage={agoraStageModeStore.inviteToStage}
+                    isInviteDialogShown={collaborationStore.inviteOnStageDialog.isOpen}
+                    openInviteDialog={collaborationStore.inviteOnStageDialog.open}
+                    closeInviteDialog={collaborationStore.inviteOnStageDialog.close}
+                  />
+                ))
+              : agoraMeetingStore.users.map((user) => (
+                  <MeetingUser
+                    key={user.uid}
+                    spaceId={space?.id || ''}
+                    user={user}
+                    isModerator={collaborationStore.isModerator}
+                    maxVideoStreams={agoraMeetingStore.maxVideoStreamsReached}
+                    onMuteUser={meetingRoomStore.muteUser}
+                    onKickUser={meetingRoomStore.kickUser}
+                  />
+                ))}
+          </ul>
+        </styled.ListItemContent>
+      </styled.ListItem>
+    </styled.Container>
   );
 };
 
