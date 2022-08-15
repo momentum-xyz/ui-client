b0c7b28b05fccf5daafc0c3218f070340d3728e3
andrey_kovalev_s <andrey.kovalev@perfectart.com>
2022-08-15 19:49:06 +0300

    feature(unity pause): fixing

--- src/scenes/unity/pages/UnityPage/components/atoms/PathObserver/PathObserver.tsx
+++ src/scenes/unity/pages/UnityPage/components/atoms/PathObserver/PathObserver.tsx
@@ -1,8 +1,10 @@
-import React, {FC, useCallback, useEffect, useRef} from 'react';
+import React, {FC, useCallback, useEffect} from 'react';
 import {observer} from 'mobx-react-lite';
 import {useLocation} from 'react-router-dom';
 
 import {ROUTES} from 'core/constants';
+import {isTargetRoute} from 'core/utils';
+import {RouteConfigInterface} from 'core/interfaces';
 
 interface PropsInterface {
   isTeleportReady: boolean;
@@ -12,34 +14,38 @@ interface PropsInterface {
 
 const UNITY_WAITING_MS = 200;
 
-const PathObserver: FC<PropsInterface> = ({isTeleportReady, resumeUnity, pauseUnity}) => {
-  const unityIsReady = useRef<boolean>(false);
+const SKIPPED_ROUTES: RouteConfigInterface[] = [
+  {
+    path: ROUTES.magic,
+    main: () => <></>
+  }
+];
 
+const PathObserver: FC<PropsInterface> = ({isTeleportReady, resumeUnity, pauseUnity}) => {
   const location = useLocation();
 
   const changeUnityState = useCallback(
     (pathname: string) => {
-      if (pathname === ROUTES.base) {
-        resumeUnity();
-      } else {
-        pauseUnity();
+      if (!isTargetRoute(pathname, SKIPPED_ROUTES)) {
+        if (pathname === ROUTES.base) {
+          resumeUnity();
+        } else {
+          pauseUnity();
+        }
       }
     },
     [pauseUnity, resumeUnity]
   );
 
   useEffect(() => {
-    changeUnityState(location.pathname);
-  }, [changeUnityState, location.pathname]);
+    const timeout = setTimeout(() => {
+      changeUnityState(location.pathname);
+    }, UNITY_WAITING_MS);
 
-  useEffect(() => {
-    if (isTeleportReady && !unityIsReady.current) {
-      unityIsReady.current = true;
-      setTimeout(() => {
-        changeUnityState(location.pathname);
-      }, UNITY_WAITING_MS);
-    }
-  }, [changeUnityState, isTeleportReady, location.pathname]);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [changeUnityState, location.pathname, isTeleportReady]);
 
   return <></>;
 };
