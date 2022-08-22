f4092d4954232b69b293c108cf53993745eabc1d
Amin Bakhshi <amin@sigbar.nl>
2022-08-22 13:53:27 +0200

    removed unused pages/stores

--- src/scenes/widgets/stores/RootWidgetStore.ts
+++ src/scenes/widgets/stores/RootWidgetStore.ts
@@ -4,13 +4,11 @@ import StakingStore from './StakingStore/StakingStore';
 import {WorldStatsStore} from './WorldStatsStore';
 import {MagicLinkStore} from './MagicLinkStore';
 import {ProfileMenuStore} from './ProfileMenuStore';
-import {TokenRulesStore} from './TokenRulesStore';
 import {ProfileStore} from './ProfileStore';
 import {HelpStore} from './HelpStore';
 import {LaunchInitiativeStore} from './LaunchInitiativeStore';
 import {MusicPlayerStore} from './MusicPlayerStore';
 import {AttendeesListStore} from './AttendeesListStore';
-import {TokenRulesListStore} from './TokenRulesListStore';
 
 const RootWidgetStore = types.model('RootWidgetStore', {
   magicLinkStore: types.optional(MagicLinkStore, {}),
@@ -19,8 +17,6 @@ const RootWidgetStore = types.model('RootWidgetStore', {
   helpStore: types.optional(HelpStore, {}),
   profileStore: types.optional(ProfileStore, {}),
   profileMenuStore: types.optional(ProfileMenuStore, {}),
-  tokenRulesListStore: types.optional(TokenRulesListStore, {}),
-  tokenRulesStore: types.optional(TokenRulesStore, {}),
   launchInitiativeStore: types.optional(LaunchInitiativeStore, {}),
   musicPlayerStore: types.optional(MusicPlayerStore, {}),
   attendeesListStore: types.optional(AttendeesListStore, {})
