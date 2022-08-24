35b20e1710534c837cb016eab487bc51dd7c5afb
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2022-08-24 12:46:00 +0300

    style(emoji): small visual improvement

--- src/scenes/widgets/pages/EmojiWidget/EmojiWidget.styled.ts
+++ src/scenes/widgets/pages/EmojiWidget/EmojiWidget.styled.ts
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
-  width: 200px;
+  width: 192px;
+  padding-top: 10px;
 `;
 
 export const EmojiList = styled.div`
