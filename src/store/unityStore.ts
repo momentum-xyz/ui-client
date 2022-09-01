51a680ffb069467d0fb397690bce8c3a5a786991
andrey_kovalev_s <andrey.kovalev@perfectart.com>
2022-09-01 11:59:34 +0300

    feature(unity volume): initial state

--- src/store/unityStore.ts
+++ src/store/unityStore.ts
@@ -1,5 +1,5 @@
 import create from 'zustand';
 
 export const useUnityStore = create<{muted: boolean}>(() => ({
-  muted: localStorage.getItem('bg-sound-muted') === '1' ? true : false
+  muted: true
 }));
