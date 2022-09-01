bdacb66aecb379eece66d435ab5688f8bc6d9b82
andrey_kovalev_s <andrey.kovalev@perfectart.com>
2022-09-01 14:48:13 +0300

    feature(space): fetching of integrations

--- src/scenes/collaboration/stores/MiroBoardStore/MiroBoardStore.ts
+++ src/scenes/collaboration/stores/MiroBoardStore/MiroBoardStore.ts
@@ -1,7 +1,7 @@
 import {flow, types, cast} from 'mobx-state-tree';
 import {t} from 'i18next';
 
-import {api, MiroBoardInterface} from 'api';
+import {api, FetchIntegrationResponse, MiroBoardInterface} from 'api';
 import {IntegrationTypeEnum} from 'core/enums';
 import {Integration, RequestModel, ResetModel} from 'core/models';
 
@@ -14,12 +14,15 @@ const MiroBoardStore = types.compose(
     })
     .actions((self) => ({
       fetchMiroBoard: flow(function* (spaceId: string) {
-        const response = yield self.request.send(api.integrationRepository.fetchIntegration, {
-          integrationType: IntegrationTypeEnum.MIRO,
-          spaceId: spaceId
-        });
+        const response: FetchIntegrationResponse = yield self.request.send(
+          api.integrationRepository.fetchIntegration,
+          {
+            integrationType: IntegrationTypeEnum.MIRO,
+            spaceId: spaceId
+          }
+        );
 
-        if (response) {
+        if (response && self.request.isDone) {
           self.miroBoard = cast(response);
         }
       }),
