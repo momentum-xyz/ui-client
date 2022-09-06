9119306d0209c36e812bf859ff6b82947894be38
Daniel Aboo <daniel@sigbar.nl>
2022-09-06 16:52:28 +0100

    PR review fixes

--- src/scenes/auth/pages/WorldBuilderLoginPage/WorldBuilderLoginPage.tsx
+++ src/scenes/auth/pages/WorldBuilderLoginPage/WorldBuilderLoginPage.tsx
@@ -7,18 +7,16 @@ import {useStore} from 'shared/hooks';
 import {LoginView, NetworkButton, Page, PanelLayout, Text} from 'ui-kit';
 import background from 'static/images/worldBuilder.png';
 import momentum from 'static/images/momentum.svg';
-import walletConnect from 'static/images/walletConnect.svg';
 import polkadot from 'static/images/polkadot.svg';
-import metamask from 'static/images/metamask.svg';
 import {LoginTypeEnum} from 'core/enums';
 
 import {WorldBuilderWalletError} from './components';
 import * as styled from './WorldBuilderLoginPage.styled';
 
 const WorldBuilderLoginPage: FC = () => {
-  const {loginStore, web3ConnectorsFiltered} = useStore().authStore;
+  const {loginStore} = useStore().authStore;
   const {isRefreshButtonShown, isSessionExpired, errorMessage} = loginStore;
-  const {isWeb3LoginStarted, isGuestLoginStarted} = loginStore;
+  const {isWeb3LoginStarted} = loginStore;
 
   const theme = useTheme();
   const {t} = useTranslation();
@@ -34,12 +32,6 @@ const WorldBuilderLoginPage: FC = () => {
       loginStore.web3SignIn();
     }
   }, [isWeb3LoginStarted, loginStore]);
-
-  useEffect(() => {
-    if (isGuestLoginStarted) {
-      loginStore.guestSignIn();
-    }
-  }, [isGuestLoginStarted, loginStore]);
 
   return (
     <Page backgroundSrc={background}>
@@ -62,24 +54,17 @@ const WorldBuilderLoginPage: FC = () => {
               <WorldBuilderWalletError error={errorMessage} />
             ) : (
               <styled.Networks>
-                {web3ConnectorsFiltered.map((connector) => {
-                  let imageSrc = polkadot;
-                  if (connector.name === LoginTypeEnum.Metamask) {
-                    imageSrc = metamask;
-                  } else if (connector.name === LoginTypeEnum.WalletConnect) {
-                    imageSrc = walletConnect;
+                <NetworkButton
+                  theme={theme}
+                  imageSrc={polkadot}
+                  label={t(`networks.polkadot`)}
+                  onClick={() =>
+                    loginStore.chooseNetwork({
+                      name: LoginTypeEnum.Polkadot,
+                      connector: null
+                    })
                   }
-
-                  return (
-                    <NetworkButton
-                      key={connector.name}
-                      theme={theme}
-                      imageSrc={imageSrc}
-                      label={t(`networks.${connector.name}`)}
-                      onClick={() => loginStore.chooseNetwork(connector)}
-                    />
-                  );
-                })}
+                />
                 <styled.RemarksContainer>
                   <Text theme={theme} size="l" text={t('messages.startBuilding')} weight="light" />
                 </styled.RemarksContainer>
