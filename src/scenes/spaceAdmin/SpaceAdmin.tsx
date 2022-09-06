e377f484d8b508d90a90c35d060a3e710371ef01
andrey_kovalev_s <andrey.kovalev@perfectart.com>
2022-09-06 15:55:09 +0300

    feature(space admin): routes

--- src/scenes/spaceAdmin/SpaceAdmin.tsx
+++ src/scenes/spaceAdmin/SpaceAdmin.tsx
@@ -1,21 +1,18 @@
 import React, {FC, useEffect} from 'react';
-import {generatePath, useParams, useRouteMatch} from 'react-router-dom';
+import {useParams} from 'react-router-dom';
 import {observer} from 'mobx-react-lite';
 
 import {Navigation} from 'ui-kit';
 import {useStore} from 'shared/hooks';
-import {ROUTES} from 'core/constants';
 import {createSwitchByConfig} from 'core/utils';
-import {NavigationTabInterface} from 'core/interfaces';
 
-import {ADMIN_ROUTES} from './SpaceAdmin.routes';
+import {ADMIN_ROUTES, buildAdminNavigationTabs} from './SpaceAdmin.routes';
 
 const SpaceAdmin: FC = () => {
   const {spaceAdminStore} = useStore();
   const {spaceManagerStore} = spaceAdminStore;
 
   const {spaceId} = useParams<{spaceId: string}>();
-  const {path} = useRouteMatch();
 
   useEffect(() => {
     spaceManagerStore.init(spaceId);
@@ -25,27 +22,10 @@ const SpaceAdmin: FC = () => {
     };
   }, [spaceId, spaceManagerStore]);
 
-  const tabs: NavigationTabInterface[] = [
-    {
-      path: generatePath(ROUTES.spaceAdmin.base, {spaceId}),
-      iconName: 'tiles',
-      canGoBack: true,
-      replace: true,
-      exact: true
-    },
-    {
-      path: generatePath(ROUTES.spaceAdmin.broadcast, {spaceId}),
-      iconName: 'airport-signal',
-      canGoBack: true,
-      replace: true,
-      exact: true
-    }
-  ];
-
   return (
     <>
-      <Navigation tabs={tabs} />
-      {createSwitchByConfig(ADMIN_ROUTES(path))}
+      <Navigation tabs={buildAdminNavigationTabs(spaceId)} />
+      {createSwitchByConfig(ADMIN_ROUTES)}
     </>
   );
 };
