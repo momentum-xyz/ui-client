9a8f296e63b6f00713769c91d409815dbef6d0ad
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2022-09-13 14:52:21 +0300

    fix(emoji): removed the console logs
    #AT-75

--- src/api/repositories/emojiRepository/emojiRepository.api.ts
+++ src/api/repositories/emojiRepository/emojiRepository.api.ts
@@ -25,10 +25,6 @@ export const fetchSpaceEmojiConfig: RequestInterface<
   // FIXME remove after October 2022
   const testSample = (resp.data as EmojiConfigLegacyResponse)[0];
   if (typeof testSample?.emoji === 'object') {
-    console.log(
-      'LEGACY emoji format detected. Convert to newer one. Original:',
-      JSON.stringify(resp.data)
-    );
     resp.data = (resp.data as EmojiConfigLegacyResponse).map(
       ({emoji, emojiId, order, spaceId}) => ({
         ...emoji,
@@ -37,7 +33,6 @@ export const fetchSpaceEmojiConfig: RequestInterface<
         spaceId: bytesToUuid(spaceId.data)
       })
     );
-    console.log('Converted emoji data:', JSON.stringify(resp.data));
   }
 
   return resp;
