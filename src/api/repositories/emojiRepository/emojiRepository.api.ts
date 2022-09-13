62712b9972531171e0b0434f9abad40e60aa4803
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2022-09-13 14:17:22 +0300

    feature(emoji): support new and legacy format for loading emojis for space
    #AT-75

--- src/api/repositories/emojiRepository/emojiRepository.api.ts
+++ src/api/repositories/emojiRepository/emojiRepository.api.ts
@@ -2,16 +2,43 @@ import {generatePath} from 'react-router-dom';
 
 import {RequestInterface} from 'api/interfaces';
 import {request} from 'api/request';
+import {bytesToUuid} from 'core/utils';
 
 import {emojiRepositoryEndpoints} from './emojiRepository.api.endpoints';
-import {EmojiConfigRequest, EmojiConfigResponse} from './emojiRepository.api.types';
+import {
+  EmojiConfigLegacyResponse,
+  EmojiConfigRequest,
+  EmojiConfigResponse
+} from './emojiRepository.api.types';
 
-export const fetchSpaceEmojiConfig: RequestInterface<EmojiConfigRequest, EmojiConfigResponse> = (
-  options
-) => {
+export const fetchSpaceEmojiConfig: RequestInterface<
+  EmojiConfigRequest,
+  EmojiConfigResponse
+> = async (options) => {
   const {worldId, ...restOptions} = options;
 
   const url = generatePath(emojiRepositoryEndpoints().spaceEmojiConfig, {worldId});
 
-  return request.get(url, restOptions);
+  const resp = await request.get(url, restOptions);
+
+  // temp support for legacy format
+  // FIXME remove after October 2022
+  const testSample = (resp.data as EmojiConfigLegacyResponse)[0];
+  if (typeof testSample?.emoji === 'object') {
+    console.log(
+      'LEGACY emoji format detected. Convert to newer one. Original:',
+      JSON.stringify(resp.data)
+    );
+    resp.data = (resp.data as EmojiConfigLegacyResponse).map(
+      ({emoji, emojiId, order, spaceId}) => ({
+        ...emoji,
+        id: bytesToUuid(emojiId.data),
+        order,
+        spaceId: bytesToUuid(spaceId.data)
+      })
+    );
+    console.log('Converted emoji data:', JSON.stringify(resp.data));
+  }
+
+  return resp;
 };
