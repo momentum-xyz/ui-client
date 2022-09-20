36d3f75d73bed9a515ee691407e3d3140d485d41
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2022-09-20 09:46:16 +0300

    refactor(emoji): use react-hook-form for UploadEmojiDialog
    #AT-69

--- src/scenes/spaceAdmin/pages/SpaceAdminPage/components/organisms/UploadEmojiDialog/UploadEmojiDialog.tsx
+++ src/scenes/spaceAdmin/pages/SpaceAdminPage/components/organisms/UploadEmojiDialog/UploadEmojiDialog.tsx
@@ -1,12 +1,14 @@
-import {FC, useState} from 'react';
+import {FC} from 'react';
 import {useTranslation} from 'react-i18next';
 import {useTheme} from 'styled-components';
 import {toast} from 'react-toastify';
 import cn from 'classnames';
 import {observer} from 'mobx-react-lite';
+import {Controller, SubmitHandler, useForm} from 'react-hook-form';
 
 import {Dialog, FileUploader, Input, ToastContent} from 'ui-kit';
 import {useStore} from 'shared/hooks';
+import {UploadEmojiRequest} from 'api';
 
 import * as styled from './UploadEmojiDialog.styled';
 
@@ -15,6 +17,8 @@ interface PropsInterface {
   existingEmojiId: string | undefined;
 }
 
+type FormType = Omit<UploadEmojiRequest, 'spaceId'>;
+
 const UploadEmojiDialog: FC<PropsInterface> = ({spaceId, existingEmojiId}) => {
   const {uploadDialog, isUploadPending, uploadEmojiToSpace, deleteEmoji} =
     useStore().spaceAdminStore.manageEmojiStore;
@@ -22,33 +26,31 @@ const UploadEmojiDialog: FC<PropsInterface> = ({spaceId, existingEmojiId}) => {
   const theme = useTheme();
   const {t} = useTranslation();
 
-  const [name, setName] = useState<string>('');
-  const [nameError, setNameError] = useState(false);
-
-  const [image, setImage] = useState<File | null>(null);
-  const [imageError, setImageError] = useState(false);
-
-  const handleSubmit = async () => {
-    if (!image) {
-      setImageError(true);
-      return;
+  const {
+    control,
+    formState: {errors},
+    handleSubmit,
+    setError
+  } = useForm<FormType>({
+    defaultValues: {
+      name: ''
     }
-    if (!name) {
-      setNameError(true);
-      return;
-    }
+  });
 
+  const formSubmitHandler: SubmitHandler<FormType> = async ({file, name}) => {
     try {
-      setImageError(false);
-      setNameError(false);
-
       if (existingEmojiId) {
         await deleteEmoji(spaceId, existingEmojiId);
       }
-      await uploadEmojiToSpace(spaceId, image, name);
+
+      await uploadEmojiToSpace(spaceId, file, name);
+
       uploadDialog.close();
     } catch (err) {
       console.error(err);
+      setError('file', {
+        type: 'submit'
+      });
       toast.error(
         <ToastContent
           isDanger
@@ -68,44 +70,60 @@ const UploadEmojiDialog: FC<PropsInterface> = ({spaceId, existingEmojiId}) => {
       onClose={uploadDialog.close}
       approveInfo={{
         title: t('spaceAdmin.manageEmoji.uploadDialog.confirmButton'),
-        onClick: handleSubmit,
+        onClick: handleSubmit(formSubmitHandler),
         disabled: isUploadPending
       }}
     >
       <styled.DialogContainer>
-        <Input
-          label={t('spaceAdmin.manageEmoji.uploadDialog.emojiName')}
-          type="text"
-          onChange={setName}
-          errorMessage={t('spaceAdmin.manageEmoji.uploadDialog.errorMissingName')}
-          isError={nameError}
-          disabled={isUploadPending}
-        />
-        <styled.ImageUploadContainer className={cn(imageError && 'error')}>
-          {!!image && (
-            <styled.PreviewImageHolder>
-              <styled.Image src={URL.createObjectURL(image)} alt="Preview emoji" />
-            </styled.PreviewImageHolder>
+        <Controller
+          name="name"
+          control={control}
+          rules={{required: true}}
+          render={({field: {value, onChange}}) => (
+            <Input
+              label={t('spaceAdmin.manageEmoji.uploadDialog.emojiName')}
+              type="text"
+              value={value}
+              onChange={(value) => {
+                onChange(value);
+              }}
+              errorMessage={t('spaceAdmin.manageEmoji.uploadDialog.errorMissingName')}
+              isError={!!errors.name}
+              disabled={isUploadPending}
+            />
           )}
-          <FileUploader
-            theme={theme}
-            label={
-              image
-                ? t('spaceAdmin.manageEmoji.uploadDialog.changeImage')
-                : t('spaceAdmin.manageEmoji.uploadDialog.selectImage')
-            }
-            dragActiveLabel={t('fileUploader.dragActiveLabel')}
-            onFilesUpload={(file) => {
-              setImageError(false);
-              setImage(file || null);
-            }}
-            onError={(err) => {
-              console.log('File upload error:', err);
-              setImageError(true);
-            }}
-            fileType="image"
-          />
-        </styled.ImageUploadContainer>
+        />
+        <Controller
+          name="file"
+          control={control}
+          rules={{required: true}}
+          render={({field: {value, onChange}}) => (
+            <styled.ImageUploadContainer className={cn(!!errors.file && 'error')}>
+              {!!value && (
+                <styled.PreviewImageHolder>
+                  <styled.Image src={URL.createObjectURL(value)} alt="Preview emoji" />
+                </styled.PreviewImageHolder>
+              )}
+              <FileUploader
+                theme={theme}
+                label={
+                  value
+                    ? t('spaceAdmin.manageEmoji.uploadDialog.changeImage')
+                    : t('spaceAdmin.manageEmoji.uploadDialog.selectImage')
+                }
+                dragActiveLabel={t('fileUploader.dragActiveLabel')}
+                onFilesUpload={(file) => {
+                  onChange(file || null);
+                }}
+                onError={(err) => {
+                  console.log('File upload error:', err);
+                  setError('file', {message: 'upload'});
+                }}
+                fileType="image"
+              />
+            </styled.ImageUploadContainer>
+          )}
+        />
       </styled.DialogContainer>
     </Dialog>
   );
