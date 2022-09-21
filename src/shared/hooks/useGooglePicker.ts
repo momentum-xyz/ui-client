e493713be03b81c4ec63bf1774d7f946d8f2ebc9
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2022-09-21 19:28:54 +0300

    refactor(google-picker): remove "any" or add exception
    #BUG-1

--- src/shared/hooks/useGooglePicker.ts
+++ src/shared/hooks/useGooglePicker.ts
@@ -4,24 +4,27 @@ import {appVariables} from 'api/constants';
 
 const DOCUMENT_VIEW_ID = 'DOCS';
 
-export const useGooglePicker = (onChange: (data: any) => void) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const windowWithGapi: Window & {gapi?: any; google?: any} = window;
+
+export const useGooglePicker = (onChange: (data: unknown) => void) => {
   const scriptLoadingStarted = useRef<boolean>(false);
 
   const onApiLoad = () => {
-    (window as any).gapi.load('auth');
-    (window as any).gapi.load('picker');
+    windowWithGapi.gapi.load('auth');
+    windowWithGapi.gapi.load('picker');
   };
 
   const isGoogleReady = () => {
-    return !!(window as any).gapi;
+    return !!windowWithGapi.gapi;
   };
 
   const isGoogleAuthReady = () => {
-    return !!(window as any).gapi.auth;
+    return !!windowWithGapi.gapi.auth;
   };
 
   const isGooglePickerReady = () => {
-    return !!(window as any).google.picker;
+    return !!windowWithGapi.google.picker;
   };
 
   const loadScript = (src: string, onLoad: () => void) => {
@@ -30,7 +33,7 @@ export const useGooglePicker = (onChange: (data: any) => void) => {
     script.async = true;
     script.defer = true;
     script.addEventListener(`load`, onLoad);
-    (document.head as any).appendChild(script);
+    document.head.appendChild(script);
   };
 
   useEffect(() => {
@@ -42,19 +45,19 @@ export const useGooglePicker = (onChange: (data: any) => void) => {
     }
   }, []);
 
-  const doAuth = (callback: (data: any) => void) => {
+  const doAuth = (callback: (data: {access_token?: string}) => void) => {
     const config = {
       client_id: appVariables.GOOGLE_API_CLIENT_ID,
       scope: [appVariables.GOOGLE_DOCUMENT_SCOPE],
       immediate: false
     };
 
-    (window as any).gapi.auth.authorize(config, callback);
+    windowWithGapi.gapi.auth.authorize(config, callback);
   };
 
   const handleCreatePicker = (oauthToken: string) => {
-    const googleViewId = (window as any).google.picker.ViewId[DOCUMENT_VIEW_ID];
-    const view = new (window as any).google.picker.View(googleViewId);
+    const googleViewId = windowWithGapi.google.picker.ViewId[DOCUMENT_VIEW_ID];
+    const view = new windowWithGapi.google.picker.View(googleViewId);
 
     view.setMimeTypes([]);
 
@@ -62,7 +65,7 @@ export const useGooglePicker = (onChange: (data: any) => void) => {
       throw new Error("Can't find view by viewId");
     }
 
-    const picker = new (window as any).google.picker.PickerBuilder()
+    const picker = new windowWithGapi.google.picker.PickerBuilder()
       .addView(view)
       .setOAuthToken(oauthToken)
       .setDeveloperKey(appVariables.GOOGLE_API_DEVELOPER_KEY)
@@ -76,13 +79,13 @@ export const useGooglePicker = (onChange: (data: any) => void) => {
       return;
     }
 
-    const token = (window as any).gapi.auth.getToken();
+    const token = windowWithGapi.gapi.auth.getToken();
     const oauthToken = token && token.access_token;
 
     if (oauthToken) {
       handleCreatePicker(oauthToken);
     } else {
-      doAuth((response: any) => {
+      doAuth((response) => {
         if (response.access_token) {
           handleCreatePicker(response.access_token);
         } else {
