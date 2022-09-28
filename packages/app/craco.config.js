971c2b895a0986d43886509ebb9a80eb8b30a0c9
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2022-09-28 12:51:22 +0300

    build(app): restore the old craco config

--- packages/app/craco.config.js
+++ packages/app/craco.config.js
@@ -1,9 +1,4 @@
-const path = require('path');
 const CompressionPlugin = require('compression-webpack-plugin');
-const {getLoader, loaderByName} = require('@craco/craco');
-
-const packages = [];
-packages.push(path.join(__dirname, '../ui-kit'));
 
 module.exports = {
   style: {
@@ -18,27 +13,17 @@ module.exports = {
         algorithm: 'gzip'
       })
     ],
-    configure: (webpackConfig, arg) => {
-      const {isFound, match} = getLoader(webpackConfig, loaderByName('babel-loader'));
-      if (isFound) {
-        const include = Array.isArray(match.loader.include)
-          ? match.loader.include
-          : [match.loader.include];
-
-        match.loader.include = include.concat(packages);
-      }
-
-      return webpackConfig;
-    },
-    ignoreWarnings: [
-      function ignoreSourcemapsloaderWarnings(warning) {
-        return (
-          warning.module &&
-          warning.module.resource.includes('node_modules') &&
-          warning.details &&
-          warning.details.includes('source-map-loader')
-        );
-      }
-    ]
+    configure: {
+      ignoreWarnings: [
+        function ignoreSourcemapsloaderWarnings(warning) {
+          return (
+            warning.module &&
+            warning.module.resource.includes('node_modules') &&
+            warning.details &&
+            warning.details.includes('source-map-loader')
+          );
+        }
+      ]
+    }
   }
 };
