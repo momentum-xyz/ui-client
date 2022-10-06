af699e2ae4f6e6635b5d461adbfc43b097c1c525
Daniel Aboo <daniel@sigbar.nl>
2022-10-06 12:25:30 +0200

    feature(app): Removed cookies and remove miro from integration types

--- packages/app/src/core/types/Integration.type.ts
+++ packages/app/src/core/types/Integration.type.ts
@@ -1,1 +1,2 @@
-export type IntegrationType = 'miro' | 'google_drive' | 'broadcast' | 'stage_mode';
+// TODO: Remove each type after extracting the corresponding plugin
+export type IntegrationType = 'google_drive' | 'broadcast' | 'stage_mode';
