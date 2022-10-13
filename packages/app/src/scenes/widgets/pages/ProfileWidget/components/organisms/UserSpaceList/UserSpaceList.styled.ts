bbb0e94723c20929f81d21539d8bca9ea397b84f
andrey_kovalev_s <andrey.kovalev@perfectart.com>
2022-10-13 12:32:53 +0300

    feature(Profile widget): styling

--- packages/app/src/scenes/widgets/pages/ProfileWidget/components/organisms/UserSpaceList/UserSpaceList.styled.ts
+++ packages/app/src/scenes/widgets/pages/ProfileWidget/components/organisms/UserSpaceList/UserSpaceList.styled.ts
@@ -4,15 +4,11 @@ import {Text} from '@momentum-xyz/ui-kit';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100%;
-  padding-top: 10px;
-  overflow: hidden;
+  overflow-y: auto;
 `;
 
 export const Body = styled.div`
   max-height: 100%;
-  overflow-y: scroll;
-  width: calc(100% + 2px);
 `;
 
 export const Placeholder = styled(Text)`
