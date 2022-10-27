839cea4150d6ac511e711b368db6f1cb3a902a02
andrey_kovalev_s <andrey.kovalev@perfectart.com>
2022-10-27 16:55:28 +0300

    feature(spaceOptionRepository): defined

--- packages/app/src/api/repositories/spaceRepository/spaceRepository.api.ts
+++ packages/app/src/api/repositories/spaceRepository/spaceRepository.api.ts
@@ -1,21 +1,24 @@
 import {RequestInterface} from '@momentum-xyz/core';
-//import {generatePath} from 'react-router-dom';
 
 import {request} from 'api/request';
-//import {getSpaceSubAttribute} from 'api/repositories/spaceAttributeRepository';
+import {GetSpaceSubAttributeRequest, GetSpaceSubAttributeResponse} from 'api';
+import {getSpaceSubAttribute} from 'api/repositories/spaceAttributeRepository';
 
-import {FetchSpaceRequest, FetchSpaceResponse} from './spaceRepository.api.types';
+import {FetchSpaceRequest} from './spaceRepository.api.types';
 
-export const fetchSpace: RequestInterface<FetchSpaceRequest, FetchSpaceResponse> = (options) => {
+export const fetchSpace: RequestInterface<FetchSpaceRequest, GetSpaceSubAttributeResponse> = (
+  options
+) => {
   const {spaceId, ...restOptions} = options;
 
-  // TRANSFORM DATA
+  const attributeOptions: GetSpaceSubAttributeRequest = {
+    worldId: '',
+    spaceId: '',
+    plugin_id: '',
+    attribute_name: '',
+    sub_attribute_key: '',
+    ...restOptions
+  };
 
-  // CALL ANOTHER REPO
-  //const x = getSpaceSubAttribute(options, options);
-
-  // TRANSFORM DATA
-  //const url = generatePath(spaceOptionRepositoryEndpoints().options, {worldId, spaceId});
-
-  return request.get('', restOptions);
+  return getSpaceSubAttribute(attributeOptions, request);
 };
