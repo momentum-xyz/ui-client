658ce3bd6b8c77757227e00bb24f4b119eab03fb
Amin Bakhshi <amin@sigbar.nl>
2022-11-24 15:52:58 +0100

    feature(app): upload image EP fixed

--- packages/app/src/api/repositories/mediaRepository/mediaRepository.api.endpoints.ts
+++ packages/app/src/api/repositories/mediaRepository/mediaRepository.api.endpoints.ts
@@ -1,9 +1,9 @@
 import {appVariables} from 'api/constants';
 
 export const mediaRepositoryEndpoints = () => {
-  const BASE_URL = `${appVariables.BACKEND_API_URL}/profile`;
+  const BASE_URL = `${appVariables.BACKEND_API_URL}/media/upload`;
 
   return {
-    uploadImage: `${BASE_URL}/avatar`
+    uploadImage: `${BASE_URL}/image`
   };
 };
