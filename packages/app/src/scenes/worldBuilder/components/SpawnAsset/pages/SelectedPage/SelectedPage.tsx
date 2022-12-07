5c45204c1ffb493fad65e06edb2d3d809f8bbc2b
Daniel Aboo <daniel@sigbar.nl>
2022-12-07 15:36:06 +0100

    feature(app): Implemented layout of SelectedPage

--- packages/app/src/scenes/worldBuilder/components/SpawnAsset/pages/SelectedPage/SelectedPage.tsx
+++ packages/app/src/scenes/worldBuilder/components/SpawnAsset/pages/SelectedPage/SelectedPage.tsx
@@ -1,6 +1,7 @@
 import {observer} from 'mobx-react-lite';
-import {FC} from 'react';
-import {useParams} from 'react-router-dom';
+import {FC, useState} from 'react';
+import {useHistory, useParams} from 'react-router-dom';
+import {Button, Text} from '@momentum-xyz/ui-kit';
 
 import {Asset3dCategoryEnum} from 'api/enums';
 import {useStore} from 'shared/hooks';
@@ -8,21 +9,61 @@ import {useStore} from 'shared/hooks';
 import * as styled from './SelectedPage.styled';
 
 export const SelectedPage: FC = () => {
-  const {worldBuilderStore} = useStore();
+  const {worldBuilderStore, mainStore} = useStore();
   const {worldBuilderAssets3dStore} = worldBuilderStore;
+  const {unityStore} = mainStore;
 
-  const {worldId, assetCategory} = useParams<{
+  const {selectedAssset: asset} = worldBuilderAssets3dStore;
+
+  const history = useHistory();
+
+  const [, setIsVisibleInNavigation] = useState(false);
+  const [objectName, setObjectName] = useState('');
+
+  const {assetCategory} = useParams<{
     worldId: string;
     assetCategory: Asset3dCategoryEnum;
   }>();
 
-  // TODO: Implement correctly
+  if (!asset) {
+    return null;
+  }
+
   return (
     <styled.Container>
-      <h1>
-        {assetCategory} + {worldId}
-      </h1>
-      <p>{worldBuilderAssets3dStore.selectedAssset?.name}</p>
+      <styled.Image src={asset.image} />
+      {assetCategory === Asset3dCategoryEnum.CUSTOM ? (
+        <styled.NameLabel text={asset.name} size="m" />
+      ) : (
+        <styled.NameInput
+          placeholder="ObjectName"
+          onFocus={() => unityStore.changeKeyboardControl(false)}
+          onBlur={() => unityStore.changeKeyboardControl(true)}
+          onChange={setObjectName}
+        />
+      )}
+      <styled.CheckBoxLabel>
+        <styled.CheckBox
+          type="checkbox"
+          onChange={(target) => setIsVisibleInNavigation(target.currentTarget.checked)}
+        />
+        <Text text="Visible in Navigation" size="m" weight="light" />
+      </styled.CheckBoxLabel>
+      {assetCategory === Asset3dCategoryEnum.CUSTOM && (
+        <styled.NameInput
+          placeholder="Name your Object (Navigation)"
+          onFocus={() => unityStore.changeKeyboardControl(false)}
+          onBlur={() => unityStore.changeKeyboardControl(true)}
+          onChange={setObjectName}
+        />
+      )}
+      <Button label="Spawn Object" disabled={!objectName} />
+      <Button
+        label="Go back"
+        onClick={() => {
+          history.goBack();
+        }}
+      />
     </styled.Container>
   );
 };
