549b7c24975956fdf1ca72ca61fe6e882b6c7728
FIZLIN <akostov94@gmail.com>
2022-12-19 14:03:52 +0200

    feat(get-tokens): Renamed constants file;

--- packages/app/src/core/constants/index.ts
+++ packages/app/src/core/constants/index.ts
@@ -5,4 +5,4 @@ export * from './dashboard.constants';
 export * from './eventEmitters.constants';
 export * from './unity.constants';
 export * from './tiles.constants';
-export * from './lastAirdrop.constants';
+export * from './blockchain.constants';
