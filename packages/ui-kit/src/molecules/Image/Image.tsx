84750eda040fcc5040448096095b6c1d91f09c33
Daniel Aboo <daniel@sigbar.nl>
2022-12-20 08:20:16 +0000

    quick-fix(ui-kit): fixed build failing

--- packages/ui-kit/src/molecules/Image/Image.tsx
+++ packages/ui-kit/src/molecules/Image/Image.tsx
@@ -4,9 +4,6 @@ import defaultLoaderPlaceholder from '../../static/images/spinner.svg';
 import defaultErrorPlaceholder from '../../static/images/astronaut.svg';
 import {ComponentSizeInterface} from '../../interfaces';
 
-import defaultErrorPlaceholder from '../../assets/images/common/astronaut.svg';
-import defaultLoaderPlaceholder from '../../assets/images/common/spinner.svg';
-
 import * as styled from './Image.styled';
 
 interface PropsInterface {
