7b1079dbe57ddfbf47accd2deb34aaf6d4b520b5
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2022-12-21 12:12:50 +0200

    fix(creator): build mode in Unity is not toggled when Creator is opened

--- packages/app/src/scenes/odysseyCreator/OdysseyCreator.tsx
+++ packages/app/src/scenes/odysseyCreator/OdysseyCreator.tsx
@@ -1,12 +1,25 @@
 import {observer} from 'mobx-react-lite';
-import {FC} from 'react';
+import {FC, useEffect} from 'react';
 
 import {createSwitchByConfig} from 'core/utils';
 import {ROUTES} from 'core/constants';
+import {UnityService} from 'shared/services';
 
 import {ODYSSEY_CREATOR_ROUTES} from './OdysseyCreator.routes';
 
 const OdysseyCreator: FC = () => {
+  useEffect(() => {
+    const isOn = UnityService.isBuildMode;
+    if (!isOn) {
+      UnityService.toggleBuildMode();
+    }
+    return () => {
+      if (isOn) {
+        UnityService.toggleBuildMode();
+      }
+    };
+  }, []);
+
   return <>{createSwitchByConfig(ODYSSEY_CREATOR_ROUTES, ROUTES.odyssey.creator.base)}</>;
 };
 
