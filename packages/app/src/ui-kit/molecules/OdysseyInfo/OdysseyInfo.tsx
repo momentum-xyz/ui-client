6ccb04981b7e89ba01da4e5634a9279b06e6e15b
FIZLIN <akostov94@gmail.com>
2022-12-21 16:48:04 +0200

    feat(odyssey-info-search-widget): removed obsolete icons;

--- packages/app/src/ui-kit/molecules/OdysseyInfo/OdysseyInfo.tsx
+++ packages/app/src/ui-kit/molecules/OdysseyInfo/OdysseyInfo.tsx
@@ -8,7 +8,7 @@ import {getImageAbsoluteUrl} from 'core/utils';
 import * as styled from './OdysseyInfo.styled';
 
 interface PropsInterface {
-  odyssey: OdysseyItemInterface;
+  odyssey: OdysseyItemInterface | null;
   alreadyConnected?: boolean;
   onVisit?: () => void;
   onHighFive?: () => void;
@@ -20,6 +20,7 @@ interface PropsInterface {
   coCreateDisabled?: boolean;
   highFiveDisabled?: boolean;
   dockDisabled?: boolean;
+  avatar?: string;
 }
 
 const OdysseyInfo: FC<PropsInterface> = ({
@@ -34,16 +35,18 @@ const OdysseyInfo: FC<PropsInterface> = ({
   connectDisabled,
   coCreateDisabled,
   highFiveDisabled,
-  dockDisabled
+  dockDisabled,
+  avatar = ''
 }) => {
   const {t} = useTranslation();
+  const image = avatar || odyssey?.image;
 
   return (
     <styled.OdysseyInfoContainer data-testid="OdysseyInfo-test">
       <styled.Container>
         <styled.TopContainer>
           <Image
-            src={getImageAbsoluteUrl(odyssey.image) || ''}
+            src={getImageAbsoluteUrl(image) || ''}
             sizeProps={{width: '90px', height: '90px'}}
             className="avatar"
           />
@@ -97,34 +100,38 @@ const OdysseyInfo: FC<PropsInterface> = ({
         </styled.TopContainer>
       </styled.Container>
 
-      {odyssey.description && (
-        <styled.Description>
-          <Text size="xxs" text={odyssey.description} align="left" />
-        </styled.Description>
+      {odyssey && (
+        <>
+          {odyssey.description && (
+            <styled.Description>
+              <Text size="xxs" text={odyssey.description} align="left" />
+            </styled.Description>
+          )}
+
+          <styled.Statistics>{t('titles.statistics')}</styled.Statistics>
+
+          <styled.StatisticsData>
+            <styled.StatisticsItem>
+              <IconSvg name="hierarchy" size="medium" />
+              <styled.StatisticsValue>
+                {t('staking.connectionAmount', {connections: odyssey.connections})}
+              </styled.StatisticsValue>
+            </styled.StatisticsItem>
+            <styled.StatisticsItem>
+              <IconSvg name="people" size="medium" />
+              <styled.StatisticsValue>
+                {t('staking.dockingAmount', {docking: odyssey.docking})}
+              </styled.StatisticsValue>
+            </styled.StatisticsItem>
+            <styled.StatisticsItem>
+              <IconSvg name="calendar" size="medium" />
+              <styled.StatisticsValue>
+                {t('staking.eventAmount', {events: odyssey.events})}
+              </styled.StatisticsValue>
+            </styled.StatisticsItem>
+          </styled.StatisticsData>
+        </>
       )}
-
-      <styled.Statistics>{t('titles.statistics')}</styled.Statistics>
-
-      <styled.StatisticsData>
-        <styled.StatisticsItem>
-          <IconSvg name="hierarchy" size="medium" />
-          <styled.StatisticsValue>
-            {t('staking.connectionAmount', {connections: odyssey.connections})}
-          </styled.StatisticsValue>
-        </styled.StatisticsItem>
-        <styled.StatisticsItem>
-          <IconSvg name="people" size="medium" />
-          <styled.StatisticsValue>
-            {t('staking.dockingAmount', {docking: odyssey.docking})}
-          </styled.StatisticsValue>
-        </styled.StatisticsItem>
-        <styled.StatisticsItem>
-          <IconSvg name="calendar" size="medium" />
-          <styled.StatisticsValue>
-            {t('staking.eventAmount', {events: odyssey.events})}
-          </styled.StatisticsValue>
-        </styled.StatisticsItem>
-      </styled.StatisticsData>
     </styled.OdysseyInfoContainer>
   );
 };
