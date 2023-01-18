0acb2026d73d5309a50fec574eb202bb80565089
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2023-01-18 17:37:36 +0200

    refactor(plugins): cleanup twitch/vimeo plugins

--- packages/plugin_vimeo/src/bootstrap.tsx
+++ packages/plugin_vimeo/src/bootstrap.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import 'static/styles/index.css';
-// import {ThemeProvider} from 'styled-components';
-// import {DefaultThemeConfig} from '@momentum-xyz/ui-kit';
 import {HostEmulator, PluginInterface} from '@momentum-xyz/sdk';
-// import '@momentum-xyz/ui-kit/dist/themes/themes';
-// import 'shared/services/i18n';
 
 import plugin from './Plugin';
 
@@ -13,9 +8,7 @@ const root = document.getElementById('root') as HTMLElement;
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <ThemeProvider theme={DefaultThemeConfig}> */}
     <HostEmulator plugin={plugin as PluginInterface} />
-    {/* </ThemeProvider> */}
   </React.StrictMode>,
   root
 );
