21f0fb7312310504ed628d255fbbd6b758a7c960
FIZLIN <akostov94@gmail.com>
2023-01-20 16:00:22 +0200

    feat(react-18): Resolved minor issue

--- packages/app/src/scenes/widgets_OLD/pages/HelpWidget/components/IntroVideo/IntroVideo.tsx
+++ packages/app/src/scenes/widgets_OLD/pages/HelpWidget/components/IntroVideo/IntroVideo.tsx
@@ -1,6 +1,6 @@
 import React, {FC} from 'react';
 import {observer} from 'mobx-react-lite';
-import YouTube from 'react-youtube';
+import YouTubeOG, {YouTubeProps} from 'react-youtube';
 
 import {appVariables} from 'api/constants';
 import {Section} from 'scenes/widgets_OLD/pages/HelpWidget/components';
@@ -8,6 +8,8 @@ import {useStore} from 'shared/hooks';
 import {HelpSectionEnum} from 'scenes/widgets_OLD/stores/HelpStore';
 
 import * as styled from './IntroVideo.styled';
+
+const YouTube = YouTubeOG as unknown as FC<YouTubeProps>;
 
 const IntroVideo: FC = () => {
   const {helpStore} = useStore().widgetStore_OLD;
