89dd51c3c3aa8af859850e9f91d80957521dd0ba
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2023-01-27 16:59:03 +0200

    fix(creator): broken default route

--- packages/app/src/scenes/odysseyCreator/OdysseyCreator.tsx
+++ packages/app/src/scenes/odysseyCreator/OdysseyCreator.tsx
@@ -1,13 +1,17 @@
+import {generatePath} from 'react-router-dom';
 import {observer} from 'mobx-react-lite';
 import {FC, useEffect} from 'react';
 
 import {createSwitchByConfig} from 'core/utils';
 import {ROUTES} from 'core/constants';
 import {UnityService} from 'shared/services';
+import {useStore} from 'shared/hooks';
 
 import {ODYSSEY_CREATOR_ROUTES} from './OdysseyCreator.routes';
 
 const OdysseyCreator: FC = () => {
+  const {unityStore} = useStore();
+  const {worldId} = unityStore;
   useEffect(() => {
     UnityService.toggleBuildMode();
     return () => {
@@ -15,7 +19,14 @@ const OdysseyCreator: FC = () => {
     };
   }, []);
 
-  return <>{createSwitchByConfig(ODYSSEY_CREATOR_ROUTES, ROUTES.odyssey.creator.base)}</>;
+  return (
+    <>
+      {createSwitchByConfig(
+        ODYSSEY_CREATOR_ROUTES,
+        generatePath(ROUTES.odyssey.creator.base, {worldId})
+      )}
+    </>
+  );
 };
 
 export default observer(OdysseyCreator);
