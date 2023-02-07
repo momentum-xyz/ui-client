2c75fa7c4a88cd40c4457b76e7eda869a9b3fb6c
andrey_kovalev_s <andrey.kovalev@perfectart.com>
2023-02-07 16:39:14 +0300

    feature(newsfeed): fixing images

--- packages/app/src/ui-kit/molecules/NewsFeedItem/components/CreatedItem/CreatedItem.tsx
+++ packages/app/src/ui-kit/molecules/NewsFeedItem/components/CreatedItem/CreatedItem.tsx
@@ -1,12 +1,11 @@
-import React, {FC, useMemo} from 'react';
+import React, {FC} from 'react';
 import {observer} from 'mobx-react-lite';
-import {format} from 'date-fns-tz';
-import {Button, Text} from '@momentum-xyz/ui-kit';
+import {Button, Image, Text} from '@momentum-xyz/ui-kit';
+import {newsfeedDateString} from '@momentum-xyz/core';
 
 import {getImageAbsoluteUrl} from 'core/utils';
 import {NftFeedItemInterface} from 'api';
 import {NftItemModelInterface, UserModelInterface} from 'core/models';
-import astronautIcon from 'static/images/astronaut-green.svg';
 
 import * as styled from './CreatedItem.styled';
 
@@ -21,23 +20,17 @@ interface PropsInterface {
 const CreatedItem: FC<PropsInterface> = (props) => {
   const {item, currentUser, onTeleport, onConnect, onOpenOdyssey} = props;
 
-  const formattedDate = useMemo(() => {
-    return format(new Date(item.date), `MM/dd/yyyy - h:mm aa`);
-  }, [item.date]);
-
   const name = item.uuid === currentUser.id ? currentUser.name : item.name;
   const image = item.uuid === currentUser.id ? currentUser.profile.avatarHash : item.image;
 
   return (
     <>
-      <div>
-        <styled.OneAvatar
-          src={getImageAbsoluteUrl(image) || astronautIcon}
-          onClick={() => onOpenOdyssey?.(item.uuid)}
-        />
-      </div>
+      <styled.OneAvatar onClick={() => onOpenOdyssey?.(item.uuid)}>
+        <Image src={getImageAbsoluteUrl(image)} sizeProps={{width: '58px', height: '58px'}} />
+      </styled.OneAvatar>
+
       <styled.Info>
-        <styled.Date>{formattedDate}</styled.Date>
+        <styled.Date>{newsfeedDateString(item.date, true)}</styled.Date>
         <div>
           <Text size="xxs" text={`${name} was created`} align="left" />
         </div>
