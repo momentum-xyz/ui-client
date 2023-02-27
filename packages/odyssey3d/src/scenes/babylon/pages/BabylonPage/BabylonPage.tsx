c7aedc3a646fedb343d5fccd52861e2852230a88
andrey_kovalev_s <andrey.kovalev@perfectart.com>
2023-02-27 17:19:53 +0300

    feature(odyssey3d package): init

---
+++ packages/odyssey3d/src/scenes/babylon/pages/BabylonPage/BabylonPage.tsx
@@ -0,0 +1,7 @@
+import {FC} from 'react';
+
+const BabylonPage: FC = () => {
+  return <div>YEAH!!!</div>;
+};
+
+export default BabylonPage;
---
+++ packages/odyssey3d/src/scenes/babylon/pages/BabylonPage/index.ts
@@ -0,0 +1,1 @@
+export {default as BabylonPage} from './BabylonPage';
