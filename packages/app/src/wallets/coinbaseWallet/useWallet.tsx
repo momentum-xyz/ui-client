3751155c271b813068ede86140e8149e90258b8a
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2023-03-13 16:55:16 +0200

    feat(coinbase-wallet): use appVariables.WEB3_PUBLIC_RPC_URL_MAINNET for wallet connector

--- packages/app/src/wallets/coinbaseWallet/useWallet.tsx
+++ packages/app/src/wallets/coinbaseWallet/useWallet.tsx
@@ -3,14 +3,7 @@ import {useWeb3React} from '@web3-react/core';
 import {WalletLinkConnector} from '@web3-react/walletlink-connector';
 import {UseWalletType} from 'wallets/wallets.types';
 
-// TODO use appVariables
-const connector = new WalletLinkConnector({
-  url: `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
-  appName: 'Odyssey App',
-  supportedChainIds: [1, 3, 4, 5, 42]
-});
-
-export const useWallet: UseWalletType = () => {
+export const useWallet: UseWalletType = ({appVariables}) => {
   const {library, account, activate, deactivate, active} = useWeb3React();
   console.log('CoinbaseWallet useWallet', {library, account, activate, active});
 
@@ -24,8 +17,15 @@ export const useWallet: UseWalletType = () => {
   );
 
   useEffect(() => {
-    console.log('CoinbaseWallet useWallet activate');
-    // connector.activate()
+    console.log('CoinbaseWallet useWallet activate', appVariables.WEB3_PUBLIC_RPC_URL_MAINNET);
+
+    const connector = new WalletLinkConnector({
+      // url: `https://mainnet.infura.io/v3/${process.env.REACT_APP_INFURA_KEY}`,
+      url: appVariables.WEB3_PUBLIC_RPC_URL_MAINNET,
+      appName: 'Odyssey App',
+      supportedChainIds: [1, 3, 4, 5, 42]
+    });
+
     activate(connector)
       .then((res) => {
         console.log('CoinbaseWallet useWallet activated res', res);
@@ -38,7 +38,7 @@ export const useWallet: UseWalletType = () => {
       console.log('CoinbaseWallet useWallet deactivate');
       deactivate();
     };
-  }, [activate, deactivate]);
+  }, [activate, deactivate, appVariables]);
 
   return {account, accountHex: account, signChallenge};
 };
