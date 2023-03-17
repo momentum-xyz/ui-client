0a06b7535bf916af70d6e943301d82b7d54299c2
FIZLIN <akostov94@gmail.com>
2023-03-17 13:51:25 +0200

    feat(storybook): Tooltip - Lint

--- packages/ui-kit-storybook/src/atoms/Tooltip/Tooltip.stories.tsx
+++ packages/ui-kit-storybook/src/atoms/Tooltip/Tooltip.stories.tsx
@@ -19,58 +19,20 @@ const Template: Story<TooltipPropsInterface> = (args) => {
   );
 };
 
-export const Right = Template.bind({});
-Right.args = {
+export const General = Template.bind({});
+General.args = {
   text: 'Lorem ipsum'
 };
 
-export const RightOverflow = Template.bind({});
-RightOverflow.args = {
+export const GeneralOverflow = Template.bind({});
+GeneralOverflow.args = {
   text: 'Lorem ipsum dolor sit amet.'
 };
-RightOverflow.decorators = [
+GeneralOverflow.decorators = [
   () => {
     return (
       <div style={{marginTop: '50px', marginLeft: 'calc(100% - 200px)'}}>
-        <Tooltip {...RightOverflow.args}>
-          <div style={{width: '100px', background: 'black', textAlign: 'center', color: 'white'}}>
-            Hover me
-          </div>
-        </Tooltip>
-      </div>
-    );
-  }
-];
-
-export const Left = Template.bind({});
-Left.args = {
-  text: 'Lorem ipsum',
-  placement: 'left'
-};
-Left.decorators = [
-  () => {
-    return (
-      <div style={{marginTop: '50px', marginLeft: '100px'}}>
-        <Tooltip {...Left.args}>
-          <div style={{width: '100px', background: 'black', textAlign: 'center', color: 'white'}}>
-            Hover me
-          </div>
-        </Tooltip>
-      </div>
-    );
-  }
-];
-
-export const LeftOverflow = Template.bind({});
-LeftOverflow.args = {
-  text: 'Lorem ipsum dolor sit amet.',
-  placement: 'left'
-};
-LeftOverflow.decorators = [
-  () => {
-    return (
-      <div style={{marginTop: '50px', marginLeft: '100px'}}>
-        <Tooltip {...LeftOverflow.args}>
+        <Tooltip {...GeneralOverflow.args}>
           <div style={{width: '100px', background: 'black', textAlign: 'center', color: 'white'}}>
             Hover me
           </div>
