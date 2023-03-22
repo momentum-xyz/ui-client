06b9d500610cccb5b387da3b79622154666a8349
andrey_kovalev_s <andrey.kovalev@perfectart.com>
2023-03-22 15:02:30 +0300

    feature(plugins): react render

--- packages/plugin_miro/src/bootstrap.tsx
+++ packages/plugin_miro/src/bootstrap.tsx
@@ -1,22 +1,11 @@
-import React from 'react';
 import {createRoot} from 'react-dom/client';
-import 'static/styles/index.css';
-import {ThemeProvider as ThemeProviderOriginal, ThemeProviderProps} from 'styled-components';
-import {DefaultThemeConfig} from '@momentum-xyz/ui-kit';
 import {HostEmulator, PluginInterface} from '@momentum-xyz/sdk';
-import '@momentum-xyz/ui-kit/dist/themes/themes';
+
+import 'static/styles/index.css';
 
 import plugin from './Plugin';
-
-const ThemeProvider = ThemeProviderOriginal as unknown as React.FC<ThemeProviderProps<any>>;
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
 
-root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={DefaultThemeConfig}>
-      <HostEmulator plugin={plugin as PluginInterface} />
-    </ThemeProvider>
-  </React.StrictMode>
-);
+root.render(<HostEmulator plugin={plugin as PluginInterface} />);
