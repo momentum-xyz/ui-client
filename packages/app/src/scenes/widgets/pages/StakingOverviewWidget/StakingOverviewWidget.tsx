12fbb2839b247f3501712b8cb79f9f247287fd39
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2023-04-12 15:56:53 +0300

    WIP staking call

--- packages/app/src/scenes/widgets/pages/StakingOverviewWidget/StakingOverviewWidget.tsx
+++ packages/app/src/scenes/widgets/pages/StakingOverviewWidget/StakingOverviewWidget.tsx
@@ -5,36 +5,38 @@ import {useWeb3React} from '@web3-react/core';
 import Web3 from 'web3';
 import {Button} from '@momentum-xyz/ui-kit';
 
+import abi from '../../../../staking_abi.json';
+
 const contractAddress = '0x02e81060B6a7F688b3d09d57f5748636b901ade9';
 
-const abi: any[] = [
-  {
-    inputs: [
-      {
-        internalType: 'uint256',
-        name: 'num',
-        type: 'uint256'
-      }
-    ],
-    name: 'store',
-    outputs: [],
-    stateMutability: 'nonpayable',
-    type: 'function'
-  },
-  {
-    inputs: [],
-    name: 'retrieve',
-    outputs: [
-      {
-        internalType: 'uint256',
-        name: '',
-        type: 'uint256'
-      }
-    ],
-    stateMutability: 'view',
-    type: 'function'
-  }
-];
+// const abi: any[] = [
+//   {
+//     inputs: [
+//       {
+//         internalType: 'uint256',
+//         name: 'num',
+//         type: 'uint256'
+//       }
+//     ],
+//     name: 'store',
+//     outputs: [],
+//     stateMutability: 'nonpayable',
+//     type: 'function'
+//   },
+//   {
+//     inputs: [],
+//     name: 'retrieve',
+//     outputs: [
+//       {
+//         internalType: 'uint256',
+//         name: '',
+//         type: 'uint256'
+//       }
+//     ],
+//     stateMutability: 'view',
+//     type: 'function'
+//   }
+// ];
 
 const StakingOverviewWidget: FC = () => {
   const {library, account, activate, active} = useWeb3React();
@@ -54,7 +56,7 @@ const StakingOverviewWidget: FC = () => {
     getBalance();
   }, [web3, account]);
 
-  const contract = new web3.eth.Contract(abi, contractAddress);
+  const contract = new web3.eth.Contract(abi as any, contractAddress);
 
   const store = async () => {
     console.log('StakingOverviewWidget store');
@@ -68,11 +70,20 @@ const StakingOverviewWidget: FC = () => {
     console.log('StakingOverviewWidget retrieve result', result);
   };
 
+  const stake = async () => {
+    console.log('StakingOverviewWidget stake');
+    const result = await contract.methods
+      .stake('0x98e3a28d007d404bb370902d38032027', 1_000_000_042, 0)
+      .send({from: account});
+    console.log('StakingOverviewWidget stake result', result);
+  };
+
   return (
     <div>
       <Panel variant="primary" title="Staking Overview" icon="stake">
         <Button label="Store" onClick={store} />
         <Button label="Retrieve" onClick={retrieve} />
+        <Button label="Stake" onClick={stake} />
       </Panel>
     </div>
   );
