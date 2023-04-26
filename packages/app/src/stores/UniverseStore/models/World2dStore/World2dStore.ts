2f8cfa93fc80b8fbf46469dc42ca36bf70bd929b
andrey_kovalev_s <andrey.kovalev@perfectart.com>
2023-04-26 14:51:33 +0300

    feature(Odyssey): Info panel

--- packages/app/src/stores/UniverseStore/models/World2dStore/World2dStore.ts
+++ packages/app/src/stores/UniverseStore/models/World2dStore/World2dStore.ts
@@ -1,32 +1,23 @@
-import {flow, types, cast} from 'mobx-state-tree';
-import {Event3dEmitter, RequestModel, ResetModel} from '@momentum-xyz/core';
+import {types, cast} from 'mobx-state-tree';
+import {Event3dEmitter, ResetModel} from '@momentum-xyz/core';
 
-import {api, SpaceAttributeItemResponse, SpaceInterface} from 'api';
-import {mapper} from 'api/mapper';
-import {NftItemModelInterface, Object, UserDetails, WorldInfo} from 'core/models';
-import {getImageAbsoluteUrl, getRootStore} from 'core/utils';
+import {getRootStore} from 'core/utils';
+import {UserDetails, WorldDetails, WorldInfo} from 'core/models';
 
 const World2dStore = types.compose(
   ResetModel,
   types
     .model('World2dStore', {
       worldId: types.optional(types.string, ''),
+      worldDetails: types.maybeNull(WorldDetails),
       worldInfo: types.maybeNull(WorldInfo),
-      onlineUsersList: types.optional(types.array(UserDetails), []),
-
-      // TODO: Removal
-      request: types.optional(RequestModel, {}),
-      info: types.maybeNull(Object)
+      onlineUsersList: types.optional(types.array(UserDetails), [])
     })
     .actions((self) => ({
       init(worldId: string) {
         self.worldId = worldId;
-        this.loadWorld();
+        self.worldDetails = WorldDetails.create({worldId});
         this.subscribeToUsers();
-      },
-      loadWorld(): void {
-        // TODO: Implementation
-        // self.worldInfo = ...
       },
       subscribeToUsers(): void {
         Event3dEmitter.on('UserAdded', (onlineUser) => {
@@ -37,50 +28,23 @@ const World2dStore = types.compose(
         });
       },
       addOnlineUser(userId: string) {
-        const userDetails = UserDetails.create({userId});
-        self.onlineUsersList = cast([...self.onlineUsersList, userDetails]);
+        if (!self.onlineUsersList.find((u) => u.userId === userId)) {
+          const userDetails = UserDetails.create({userId});
+          self.onlineUsersList = cast([...self.onlineUsersList, userDetails]);
+        }
       },
       removeOnlineUser(userId: string) {
         self.onlineUsersList = cast([
           ...self.onlineUsersList.filter((user) => user.userId !== userId)
         ]);
-      },
-      // TODO: Removal
-      fetchWorldInformation: flow(function* (spaceId: string) {
-        const response: SpaceAttributeItemResponse = yield self.request.send(
-          api.spaceRepository.fetchSpace,
-          {spaceId}
-        );
-
-        if (response) {
-          self.info = cast({
-            id: spaceId,
-            ...mapper.mapSpaceSubAttributes<SpaceInterface>(response)
-          });
-        }
-      })
+      }
     }))
     .views((self) => ({
       get isMyWorld(): boolean {
         return self.worldId === getRootStore(self).sessionStore.userId;
-      }
-    }))
-    .views((self) => ({
+      },
       get isCurrentUserWorldAdmin(): boolean {
-        if (self.isMyWorld) {
-          return true;
-        }
-        const worldNft = getRootStore(self).nftStore.getNftByUuid(self.worldId);
-        return worldNft?.owner
-          ? getRootStore(self).nftStore.mutualStakingAddresses.includes(worldNft.owner)
-          : false;
-      },
-      get worldImageSrc(): string {
-        const worldNft = getRootStore(self).nftStore.getNftByUuid(self.worldId);
-        return worldNft?.image ? getImageAbsoluteUrl(worldNft.image) || '' : '';
-      },
-      get nftOfWorld(): NftItemModelInterface | undefined {
-        return getRootStore(self).nftStore.getNftByUuid(self.worldId);
+        return this.isMyWorld;
       }
     }))
 );
