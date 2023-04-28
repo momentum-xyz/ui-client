1ad6cd50ceebe9e03f8c8770bec21e487c2b424c
Dmitry Yudakov <dmitry-yudakov@users.noreply.github.com>
2023-04-28 10:01:59 +0300

    fix(blockchain-utils): division by zero for small values
    Added also small optimisation for the lookup table

--- packages/app/src/core/utils/blockchain.utils.ts
+++ packages/app/src/core/utils/blockchain.utils.ts
@@ -1,5 +1,18 @@
 import BN from 'bn.js';
 
+const lookup = [
+  {value: new BN('1000'), symbol: 'k'}, // kilo
+  {value: new BN('1000000'), symbol: 'M'}, // mega
+  {value: new BN('1000000000'), symbol: 'G'}, // giga
+  {value: new BN('1000000000000'), symbol: 'T'}, // tera
+  {value: new BN('1000000000000000'), symbol: 'P'}, // peta
+  {value: new BN('1000000000000000000'), symbol: 'E'}, // exa
+  {value: new BN('1000000000000000000000'), symbol: 'Z'}, // zetta
+  {value: new BN('1000000000000000000000000'), symbol: 'Y'}, // yotta
+  {value: new BN('1000000000000000000000000000'), symbol: 'R'}, // ronna
+  {value: new BN('1000000000000000000000000000000'), symbol: 'Q'} // quetta
+].reverse();
+
 export const formatBigInt = (input: string | null | undefined, digits = 2): string => {
   if (!input || input === '0') {
     return '0';
@@ -8,27 +21,13 @@ export const formatBigInt = (input: string | null | undefined, digits = 2): stri
   const inputSliced = input.slice(0, -18);
   const balance = new BN(inputSliced);
 
-  const lookup = [
-    {value: '0', symbol: ''},
-    {value: '1000', symbol: 'k'}, // kilo
-    {value: '1000000', symbol: 'M'}, // mega
-    {value: '1000000000', symbol: 'G'}, // giga
-    {value: '1000000000000', symbol: 'T'}, // tera
-    {value: '1000000000000000', symbol: 'P'}, // peta
-    {value: '1000000000000000000', symbol: 'E'}, // exa
-    {value: '1000000000000000000000', symbol: 'Z'}, // zetta
-    {value: '1000000000000000000000000', symbol: 'Y'}, // yotta
-    {value: '1000000000000000000000000000', symbol: 'R'}, // ronna
-    {value: '1000000000000000000000000000000', symbol: 'Q'} // quetta
-  ];
-
-  const lookupItem = lookup.reverse().find((i) => {
-    return balance.gte(new BN(i.value));
+  const lookupItem = lookup.find((i) => {
+    return balance.gte(i.value);
   });
 
   if (!lookupItem) {
     return balance.toString();
   }
 
-  return balance.div(new BN(lookupItem.value)).toNumber().toFixed(digits) + lookupItem.symbol;
+  return balance.div(lookupItem.value).toNumber().toFixed(digits) + lookupItem.symbol;
 };
