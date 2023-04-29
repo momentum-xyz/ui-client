f77edac88c22b767effd3299659379ad00f25cea
FIZLIN <akostov94@gmail.com>
2023-04-29 11:14:36 +0300

    feat(add-object-redesign): List tweaks;

--- packages/app/src/scenes/widgets/pages/CreatorWidget/pages/SpawnAssetPage/pages/AssetsPage/AssetsPage.styled.ts
+++ packages/app/src/scenes/widgets/pages/CreatorWidget/pages/SpawnAssetPage/pages/AssetsPage/AssetsPage.styled.ts
@@ -1,10 +1,27 @@
 import styled from 'styled-components';
 
-export const Contaier = styled.div`
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`;
+export const GridContainer = styled.div`
   display: flex;
   align-items: baseline;
   justify-content: center;
   width: 100%;
   height: 100%;
-  overflow-y: scroll;
+`;
+
+export const Header = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  font-size: 16px;
+  font-weight: 700;
+  letter-spacing: 0.2em;
+  line-height: 24px;
+  text-transform: uppercase;
+  margin-bottom: 10px;
 `;
