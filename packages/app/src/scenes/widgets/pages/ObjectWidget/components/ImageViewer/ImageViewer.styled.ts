034eccc971fe2ac089fbbc9cd54a5f851ce4bfab
andrey_kovalev_s <andrey.kovalev@perfectart.com>
2023-05-19 18:29:26 +0300

    feature(Object View): Scrolls

--- packages/app/src/scenes/widgets/pages/ObjectWidget/components/ImageViewer/ImageViewer.styled.ts
+++ packages/app/src/scenes/widgets/pages/ObjectWidget/components/ImageViewer/ImageViewer.styled.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  padding: 10px 0 0 0;
+`;
 
 export const ImageContainer = styled.div`
   margin: 10px 0 0 0;
@@ -18,7 +20,7 @@ export const PreviewImageHolder = styled.div`
   background-position: center center;
   background-size: cover;
   position: absolute;
-  width: calc(100% - 20px);
+  width: 100%;
   height: calc(100% - 20px);
-  border-radius: 8px;
+  border-radius: 4px;
 `;
