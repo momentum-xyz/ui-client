fe738383da2e6f2c5c97610cfdc173404149d742
FIZLIN <akostov94@gmail.com>
2023-05-23 16:27:20 +0300

    feat(feature-flag-newsfeed): lint

--- packages/app/src/api/constants/featureFlags.constants.ts
+++ packages/app/src/api/constants/featureFlags.constants.ts
@@ -1,7 +1,7 @@
-import { FeatureFlagEnum } from "api/enums";
+import {FeatureFlagEnum} from 'api/enums';
 
-import { appVariables } from "./app.variables";
+import {appVariables} from './app.variables';
 
 export const isFeatureEnabled = (feature: FeatureFlagEnum): boolean => {
   return !!appVariables.FEATURE_FLAGS[feature];
-}
+};
