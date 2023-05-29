36f8df04e84e29d6cbd6813c5687fa6def467905
FIZLIN <akostov94@gmail.com>
2023-05-29 18:15:08 +0300

    feat(newsfeed-storybook): Text + Image

---
+++ packages/core/src/enums/newsfeed.enum.ts
@@ -0,0 +1,15 @@
+export enum NewsfeedTypeEnum {
+  CREATED = 'created',
+  CONNECTED = 'connected',
+  CALENDAR = 'calendar',
+  BOOST = 'boost',
+  IMAGE = 'image',
+  VIDEO = 'video'
+}
+
+export interface NewsFeedMediaEntryDataInterface {
+  image: string | null;
+  video: string | null;
+  world_id: string | null;
+  comment: string | null;
+}
---
+++ packages/ui-kit/src/organisms/NewsfeedEntry/index.ts
@@ -0,0 +1,2 @@
+export {default as NewsfeedEntry} from './NewsfeedEntry';
+export * from './NewsfeedEntry';
