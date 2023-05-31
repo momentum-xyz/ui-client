04db6a5879f6f2c6b1a009fc851225714b007365
borislav <s7ubborn@gmail.com>
2023-05-31 15:03:03 +0200

    feat(babylon): Adding support for screenshot and video capturing

---
+++ packages/core3d/src/babylon/ScreenCaptureHelper.ts
@@ -0,0 +1,27 @@
+import {Scene, Tools, VideoRecorder} from '@babylonjs/core';
+
+import { PlayerHelper } from './PlayerHelper';
+
+export class ScreenCaptureHelper {
+  static scene: Scene;
+
+  static initialize(scene: Scene): void {
+    this.scene = scene;
+  }
+
+  static takeScreenshot(size: number) {
+    Tools.CreateScreenshot(this.scene.getEngine(), PlayerHelper.camera, size, (data) => {
+      // Do something with image data
+      //const imageBlob = new Blob([data]);
+    });
+  }
+
+  static recordVideo(duration: number) {
+    if (VideoRecorder.IsSupported(this.scene.getEngine())) {
+      const recorder = new VideoRecorder(this.scene.getEngine());
+      recorder.startRecording(null, duration).then((videoBlob) => {
+          // Do Something with the videoBlob.
+      });
+    }
+  }
+}
