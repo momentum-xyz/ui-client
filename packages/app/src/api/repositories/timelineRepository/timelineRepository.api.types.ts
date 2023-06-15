bd0f900e256b8137af6ea7daa7bbc0bb93f84552
andrey_kovalev_s <andrey.kovalev@perfectart.com>
2023-06-15 15:39:44 +0300

    feature(Timeline): Infinity list

--- packages/app/src/api/repositories/timelineRepository/timelineRepository.api.types.ts
+++ packages/app/src/api/repositories/timelineRepository/timelineRepository.api.types.ts
@@ -18,36 +18,6 @@ export interface TimelineItemInterface {
   };
 }
 
-/*
-* activity_id
-:
-"0188b8d8-85ac-7d2f-b015-77d05429cde9"
-
-:
-"bc1d47ca3b167b5a6c993114cd9f79f7"
-created_at
-:
-"2023-06-14T07:39:37.774945Z"
-data
-:
-{position: {x: 0, y: 0, z: 0}, description: "My screenshot #3",…}
-object_id
-:
-"00000000-0000-8000-8000-000000000001"
-type
-:
-"screenshot"
-user_id
-:
-"0187c1e8-bb1c-7fe3-9bd8-8178672e25cc"
-user_name
-:
-"kovi_mm"
-world_name
-:
-"#Luigi. He is Mario
-* */
-
 /** FETCH TIMELINE **/
 
 export interface FetchTimelineRequest {
