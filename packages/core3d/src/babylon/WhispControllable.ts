bd3febedba8720efa347f6afa0a6a0c6746ad551
Job <jobtalle@hotmail.com>
2023-06-27 14:13:54 +0200

    Defined Whisp class

---
+++ packages/core3d/src/babylon/WhispControllable.ts
@@ -0,0 +1,11 @@
+import {Whisp} from "./Whisp";
+
+export class WhispControllable extends Whisp {
+    /**
+     * Update the state
+     * @param {number} delta The time delta in seconds
+     */
+    update(delta: number) {
+        super.update(delta);
+    }
+}
