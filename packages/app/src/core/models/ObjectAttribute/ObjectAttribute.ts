604d8806bef2bf81ed12f3adff2e40a6f23db4dc
Dmitry Yudakov <dmitry.yudakov@gmail.com>
2023-08-18 19:55:59 +0300

    feat(models): ObjectAttribute model

---
+++ packages/app/src/core/models/ObjectAttribute/ObjectAttribute.ts
@@ -0,0 +1,103 @@
+import {flow, types} from 'mobx-state-tree';
+import {RequestModel} from '@momentum-xyz/core';
+import {AttributeNameEnum, AttributeValueInterface} from '@momentum-xyz/sdk';
+
+import {api} from 'api';
+import {PluginIdEnum} from 'api/enums';
+
+export const ObjectAttribute = types
+  .model('ObjectAttribute', {
+    objectId: types.string,
+    attributeName: types.optional(types.string, AttributeNameEnum.STATE),
+    pluginId: types.optional(types.string, PluginIdEnum.CORE),
+    _value: types.maybe(types.frozen<AttributeValueInterface>()),
+    request: types.optional(RequestModel, {})
+  })
+  .actions((self) => ({
+    load: flow(function* () {
+      console.log('ObjectAttribute load', {
+        spaceId: self.objectId,
+        plugin_id: self.pluginId,
+        attribute_name: self.attributeName
+      });
+      const response: AttributeValueInterface | undefined = yield self.request.send(
+        api.spaceAttributeRepository.getSpaceAttribute,
+        {
+          spaceId: self.objectId,
+          plugin_id: self.pluginId,
+          attribute_name: self.attributeName
+        }
+      );
+
+      console.log(
+        'ObjectAttribute load',
+        {
+          spaceId: self.objectId,
+          plugin_id: self.pluginId,
+          attribute_name: self.attributeName
+        },
+        'resp:',
+        response
+      );
+
+      if (response) {
+        self._value = response;
+      }
+
+      return response;
+    }),
+    set: flow(function* (value: AttributeValueInterface) {
+      console.log('ObjectAttribute set:', value);
+
+      yield self.request.send(api.spaceAttributeRepository.setSpaceAttribute, {
+        spaceId: self.objectId,
+        plugin_id: self.pluginId,
+        attribute_name: self.attributeName,
+        value
+      });
+
+      if (self.request.isError) {
+        throw new Error('Error setting attribute: ' + self.request.errorCode);
+      }
+
+      self._value = value;
+    }),
+    setItem: flow(function* (itemName: string, value: unknown) {
+      yield self.request.send(api.spaceAttributeRepository.setSpaceAttributeItem, {
+        spaceId: self.objectId,
+        plugin_id: self.pluginId,
+        attribute_name: self.attributeName,
+        sub_attribute_key: itemName,
+        value
+      });
+
+      if (self.request.isError) {
+        throw new Error('Error setting attribute item: ' + self.request.errorCode);
+      }
+
+      if (self._value) {
+        self._value[itemName] = value;
+      } else {
+        self._value = {[itemName]: value};
+      }
+    }),
+    delete: flow(function* () {
+      yield self.request.send(api.spaceAttributeRepository.deleteSpaceAttribute, {
+        spaceId: self.objectId,
+        plugin_id: self.pluginId,
+        attribute_name: self.attributeName
+      });
+
+      if (self.request.isError) {
+        throw new Error('Error deleting attribute: ' + self.request.errorCode);
+      }
+    }),
+    valueAs: <T>(): T | undefined => {
+      return self._value as T | undefined;
+    }
+  }))
+  .views((self) => ({
+    get value(): AttributeValueInterface | undefined {
+      return self._value;
+    }
+  }));
